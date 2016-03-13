0e14e64f3e5088cbce4ab354b3abf1c7c15bc20f
qianzhang <qzz0005@auburn.edu>
2016-03-12 22:13:14 -0600

    mylistview version1

---
+++ MyListView/app/src/main/java/mylistview/qianzhang/com/mylistview/MainActivity.java
@@ -0,0 +1,34 @@
+package mylistview.qianzhang.com.mylistview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private ListView listView;
+    private String[] values = new String[]{"Hello", "World","Android","Application","My Friend", "I love MoMo","HaHa", "I am Qian Zhang"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, android.R.id.text1,values);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new ListView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int intposition = position;
+                String clickedValue = listView.getItemAtPosition(intposition).toString();
+                Toast.makeText(getApplicationContext(),clickedValue,Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
